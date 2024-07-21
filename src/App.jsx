2a625cf67a00503b610cbcba96cbca7b56bd1f1d
Sagar Kumar sah Kanu <sagarsah20834@gmail.com>
2024-07-21 17:48:32 +0545

    Backend Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import { Outlet } from "react-router-dom";
-import Heropage from "./Components/Heropage";
-import vector from "../src/assets/curve.png";
-import ShareRecipe from "./Components/ShareRecipe";
-
-import data from "./CardData";
 import Footer from "./Components/Footer";
-import Breadcrumb from "./Components/Breadcrumb";
 
 function App() {
   return (
